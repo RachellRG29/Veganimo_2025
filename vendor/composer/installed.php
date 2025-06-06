a1c769d6d5a115e1dcbb55c6db2f84e29ca44048
Naquielrayne74 <nahunargueta341@gmail.com>
2025-06-05 23:42:42 -0600

    se agrego formulario crear perfil nutricional (visual)

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '5265ea0a53d7c1c07166f2bbf36e4e020224e24c',
+        'reference' => '0e1690c7eb29c4a709100ae2999e5251ad73b72e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '5265ea0a53d7c1c07166f2bbf36e4e020224e24c',
+            'reference' => '0e1690c7eb29c4a709100ae2999e5251ad73b72e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
