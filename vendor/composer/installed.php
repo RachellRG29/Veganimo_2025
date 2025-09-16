9379ccdb6ea3e86c9d464751e28cc1259659ed52
Naquielrayne74 <nahunargueta341@gmail.com>
2025-09-15 21:18:38 -0600

    Se actualizó PHPMailer para hacer merge después

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '696dac932cd35aa37caae2e5cb734ba369248639',
+        'reference' => '86f760d3850cccecf4e66ec0708e885a2a98710c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '696dac932cd35aa37caae2e5cb734ba369248639',
+            'reference' => '86f760d3850cccecf4e66ec0708e885a2a98710c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
