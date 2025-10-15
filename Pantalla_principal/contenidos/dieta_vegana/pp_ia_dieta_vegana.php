506ce79c39ff1576eed0def733490a36b9233c29
RachellRG29 <RachellRG29@gmail.com>
2025-10-15 11:23:11 -0600

    Corrección de detalles en diseño de botones con dieta vegana, inicio, plan entre otros

--- Pantalla_principal/contenidos/dieta_vegana/pp_ia_dieta_vegana.php
+++ Pantalla_principal/contenidos/dieta_vegana/pp_ia_dieta_vegana.php
@@ -9,60 +9,68 @@
     </div>
     
 
-    <!-- suscripcion activa / acabando de pagar o suscribirse  -->
-    <div class="contenedor_dieta_ia">
-        <div class="tarjeta_vg_dieta_ia">
-            <div class="contenido_tarj_dieta_ia">
-                <div class="circulo_dieta_vg_ia">
-                    <img src="../Images/avatares/sf_predeterminado.png" alt="" class="img_tarj_dieta_ia_pred">
-                </div>
-                
-                <div class="contenedor_sub_titulo">
-                    <h5 class="titulo_tarj_dieta_ia_vg">Crear mi Dieta Vegana automatizada​</h5>
-                    <h6 class="sub_dieta_ia">Basada en tu perfil nutricional</h6>
-                </div>
-                
+    <!-- SECCIÓN 1: VISUALIZACION DE DATOS PERFIL NUTRICIONAL NO SE MODIFICAN AQUI  -->
+    <section class="sect_dieta_ia">
+        <!-- suscripcion activa / acabando de pagar o suscribirse  -->
+        <div class="contenedor_dieta_ia">
+            <div class="tarjeta_vg_dieta_ia">
+                <div class="contenido_tarj_dieta_ia">
+                    <div class="circulo_dieta_vg_ia">
+                        <img src="../Images/avatares/sf_predeterminado.png" alt="" class="img_tarj_dieta_ia_pred">
+                    </div>
+                    
+                    <div class="contenedor_sub_titulo">
+                        <h5 class="titulo_tarj_dieta_ia_vg">Crear mi Dieta Vegana automatizada​</h5>
+                        <h6 class="sub_dieta_ia">Basada en tu perfil nutricional</h6>
+                    </div>
+                    
 
-                <div class="datos_dieta_ia">
-                    <div class="seccion_referencia">
-                        <p>Peso actual: <span id="peso">00</span></p>
-                        <p>Altura: <span id="altura">155cm</span></p>
-                        <p>Edad: <span id="edad">x años</span></p>
-                        <p>Género: <span id="genero">masculino/fem</span></p>
-                        <p>Dieta actual: <span id="dieta_actual">normal</span></p>
-                        <p>Meta a futuro: <span id="meta_futura">vegano</span></p>
-                        <p>Objetivo: <span id="objetivo">perder peso</span></p>
+                    <!-- Datos referencia del perfil nutricional  -->
+                    <div class="datos_dieta_ia">
+                        <div class="seccion_referencia">
+                            <p>Peso actual: <span id="peso">00</span></p>
+                            <p>Altura: <span id="altura">155cm</span></p>
+                            <p>Edad: <span id="edad">x años</span></p>
+                            <p>Género: <span id="genero">masculino/fem</span></p>
+                            <p>Dieta actual: <span id="dieta_actual">normal</span></p>
+                            <p>Meta a futuro: <span id="meta_futura">vegano</span></p>
+                            <p>Objetivo: <span id="objetivo">perder peso</span></p>
+                        </div>
+
+                        <div class="seccion_historia">
+                            <div>
+                            <h6>Historia clínica</h6>
+                            <p id="historia_clinica">Antecedentes: patológico, familiar o quirúrgico</p>
+                            </div>
+                            <div>
+                            <h6>Afecciones personales</h6>
+                            <p id="afecciones">Intolerancias / Alergias</p>
+                            </div>
+                            <div>
+                            <h6>Síntomas gastrointestinales</h6>
+                            <p id="sintomas">Síntomas</p>
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="seccion_historia">
-                        <div>
-                        <h6>Historia clínica</h6>
-                        <p id="historia_clinica">Antecedentes: patológico, familiar o quirúrgico</p>
-                        </div>
-                        <div>
-                        <h6>Afecciones personales</h6>
-                        <p id="afecciones">Intolerancias / Alergias</p>
-                        </div>
-                        <div>
-                        <h6>Síntomas gastrointestinales</h6>
-                        <p id="sintomas">Síntomas</p>
-                        </div>
+                    <h6 style="margin-bottom:20px; color: #154734;">
+                        Para poder editar estos campos, deberán ser editados dentro del portal del usuario</h6>
+
+
+
+                    <div class="botones_dieta_ia">
+                        <button class="btn_cancelar_ia"> Cancelar
+                        </button> 
+
+                        <button class="btn_crear_ia"> Crear dieta vegana
+                        </button>
                     </div>
+
+
                 </div>
-
-
-
-                <div class="botones_dieta_ia">
-                    <button class="btn_cancelar_ia"> Cancelar
-                    </button> 
-
-                    <button class="btn_crear_ia"> Crear dieta vegana
-                    </button>
-                </div>
-
-
             </div>
         </div>
-    </div>
+
+    </section>
 
 </section>
